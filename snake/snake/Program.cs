82e4621d7a0b422f5bba77894599af3b7fa450b4
TADY <alexqazpl@gmail.com>
2021-02-27 03:26:53 +0300

    First

---
+++ snake/snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+
+    }
+}
